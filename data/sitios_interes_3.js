28193290d16b8c4c159c448fd6c9c52d838c2b57
valrodriguez98 <57718556+valrodriguez98@users.noreply.github.com>
2020-03-10 18:21:34 -0500

    Initial commit

---
+++ data/sitios_interes_3.js
@@ -0,0 +1,21 @@
+var json_sitios_interes_3 = {
+"type": "FeatureCollection",
+"name": "sitios_interes_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "osm_id": "687147994", "timestamp": "2013-03-17T06:00:04.000", "name": "Fundacion", "type": "restaurant" }, "geometry": { "type": "Point", "coordinates": [ -76.536839500000013, 3.3747364 ] } },
+{ "type": "Feature", "properties": { "osm_id": "687148147", "timestamp": "2010-04-07T01:21:21.000", "name": "Plazoleta de Ingenieria", "type": "theatre" }, "geometry": { "type": "Point", "coordinates": [ -76.5328365, 3.376003 ] } },
+{ "type": "Feature", "properties": { "osm_id": "1230848643", "timestamp": "2013-03-17T15:53:13.000", "name": "Universidad Del Valle - Ciudad Universitaria Mel", "type": "university" }, "geometry": { "type": "Point", "coordinates": [ -76.534337, 3.3755525 ] } },
+{ "type": "Feature", "properties": { "osm_id": "2206266142", "timestamp": "2013-03-17T05:53:46.000", "name": "FruterÃ­a", "type": "greengrocer" }, "geometry": { "type": "Point", "coordinates": [ -76.53464, 3.3742117 ] } },
+{ "type": "Feature", "properties": { "osm_id": "2206891349", "timestamp": "2015-09-27T16:55:49.000", "name": "Los Guaduales", "type": "restaurant" }, "geometry": { "type": "Point", "coordinates": [ -76.535818800000015, 3.372286 ] } },
+{ "type": "Feature", "properties": { "osm_id": "2206904133", "timestamp": "2013-03-17T16:08:59.000", "name": "CAI, Centro de AlimentaciÃ³n Inmediata", "type": "restaurant" }, "geometry": { "type": "Point", "coordinates": [ -76.533221200000014, 3.375582 ] } },
+{ "type": "Feature", "properties": { "osm_id": "2206916348", "timestamp": "2013-03-17T16:08:59.000", "name": "Comidas RÃ¡pidas", "type": "fast_food" }, "geometry": { "type": "Point", "coordinates": [ -76.5331891, 3.3750599 ] } },
+{ "type": "Feature", "properties": { "osm_id": "2209103955", "timestamp": "2013-03-18T22:04:59.000", "name": null, "type": "bicycle_parking" }, "geometry": { "type": "Point", "coordinates": [ -76.5341203, 3.3745063 ] } },
+{ "type": "Feature", "properties": { "osm_id": "2209103959", "timestamp": "2013-03-18T22:04:59.000", "name": null, "type": "bicycle_parking" }, "geometry": { "type": "Point", "coordinates": [ -76.5326236, 3.3755185 ] } },
+{ "type": "Feature", "properties": { "osm_id": "2209103961", "timestamp": "2013-03-18T22:04:59.000", "name": null, "type": "bicycle_parking" }, "geometry": { "type": "Point", "coordinates": [ -76.5328701, 3.3755185 ] } },
+{ "type": "Feature", "properties": { "osm_id": "2209103963", "timestamp": "2013-03-18T22:04:59.000", "name": null, "type": "bicycle_parking" }, "geometry": { "type": "Point", "coordinates": [ -76.5323554, 3.3754221 ] } },
+{ "type": "Feature", "properties": { "osm_id": "2209103965", "timestamp": "2013-03-18T22:04:59.000", "name": null, "type": "bicycle_parking" }, "geometry": { "type": "Point", "coordinates": [ -76.534646, 3.3762682 ] } },
+{ "type": "Feature", "properties": { "osm_id": "2555806410", "timestamp": "2014-03-27T20:37:20.000", "name": "Origami + Art", "type": "artwork" }, "geometry": { "type": "Point", "coordinates": [ -76.5328557, 3.3754421 ] } },
+{ "type": "Feature", "properties": { "osm_id": "2747580099", "timestamp": "2014-03-27T20:37:20.000", "name": "Alcance@Roa", "type": "electronics" }, "geometry": { "type": "Point", "coordinates": [ -76.5327204, 3.3754832 ] } }
+]
+}
